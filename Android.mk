a02b9f184bac91baf3ceba5ffd2e10f944bbff83
winton.liu <admin@brahmaos.io>
2018-06-16 21:25:38 +0800

    Add ipfs
    ipfsinit succeed
    
    Signed-off-by: winton.liu <admin@brahmaos.io>

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Android.mk
+# Winton.Liu, 2018-06-11 11:47
+#
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS) 
+LOCAL_MODULE := ipfs
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
+LOCAL_INIT_RC := ipfsd.android.rc
+LOCAL_SRC_FILES := ipfs_arm
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS) 
+LOCAL_MODULE := ipfsinit
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
+LOCAL_SRC_FILES := ipfsinit
+include $(BUILD_PREBUILT)
